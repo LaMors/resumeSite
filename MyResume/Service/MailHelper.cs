190dc63349dcbc74ccd0e8c877b2d6078b86b1fb
Mortem <deusvecumest@gmail.com>
2020-12-09 22:59:11 +0200

    gitHub

--- MyResume/Service/MailHelper.cs
+++ MyResume/Service/MailHelper.cs
@@ -24,18 +24,18 @@ namespace MyResume.Service
             {
                 MimeMessage message = new MimeMessage();
                 message.From.Add(new MailboxAddress(Config.DocumentName, sender)); //message sender
-                message.To.Add(new MailboxAddress("deusvecumest@gmail.com")); //addressee of the message
-                message.Subject = subject; //тема сообщения
+                message.To.Add(new MailboxAddress("Vasyan2011@gmail.com")); //addressee of the message
+                message.Subject = subject; //Message subject
                 message.Body = new BodyBuilder() { HtmlBody = $"<div style=\"color: green;\">{name}: {"\n" + body} .My email: {sender}</div>" }.ToMessageBody(); //message body (also in HTML format)
 
                 using (SmtpClient client = new SmtpClient())
                 {
                     client.Connect("smtp.gmail.com", 465, true); // or use port 587
-                    client.Authenticate("wederkar@gmail.com", "sauda0sakdpj23u9djas"); //логин-пароль от аккаунта
+                    client.Authenticate("wawdaskar@gmail.com", "saudaasdwdwade9djas"); //login-password from the account
                     client.Send(message);
 
                     client.Disconnect(true);
-                    logger.LogInformation("Сообщение отправлено успешно!");
+                    logger.LogInformation("Message sent successfully!");
                 }
             }
             catch (Exception e)
